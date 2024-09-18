4e32f7f257440c129353863d94dfb120e7436ccc
Mahmoud Galal <mahmoudgalal173.95@gmail.com>
2024-09-18 19:17:46 +0300

    Add ui components

---
+++ Frontend/src/components/ui/Input.jsx
@@ -0,0 +1,17 @@
+const Input = ({ icon: Icon, ref, ...rest }) => {
+  return (
+    <div className="relative mb-3">
+      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        {Icon ? <Icon className="size-5 text-blue-500" /> : null}
+      </div>
+      <input
+        ref={ref}
+        className={`${
+          Icon ? "pl-10" : "pl-3"
+        } border-[1px] border-gray-300 shadow-lg focus:border-blue-600 focus:outline-none focus:ring-1 focus:ring-blue-600 rounded-lg px-3 py-3 text-md w-full bg-transparent`}
+        {...rest}
+      />
+    </div>
+  );
+};
+export default Input;
