b6904e22dac67e06a406613491fb69be177e5fed
Mahmoud Galal <mahmoudgalal173.95@gmail.com>
2024-09-18 20:25:44 +0300

    Add handle error pages

--- Frontend/src/router/index.jsx
+++ Frontend/src/router/index.jsx
@@ -8,16 +8,18 @@ import HomePage from "../pages/home/HomePage";
 import BookPage from "../components/book/BookPage";
 import SellPage from "../pages/sellBooks/SellPage";
 import RootLayout from "./../pages/Layout";
+import ErrorHandler from "./../components/errors/ErrorHandler";
+import PageNotFound from "./../pages/PageNotFound";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/" element={<RootLayout />}>
+      <Route path="/" element={<RootLayout />} errorElement={<ErrorHandler />}>
         <Route index element={<HomePage />} />
         <Route path="/book/:id" element={<BookPage />} />
         <Route path="/sellBook" element={<SellPage />} />
       </Route>
-      <Route path="*" element={<RootLayout />} />
+      <Route path="*" element={<PageNotFound />} />
     </>
   )
 );
