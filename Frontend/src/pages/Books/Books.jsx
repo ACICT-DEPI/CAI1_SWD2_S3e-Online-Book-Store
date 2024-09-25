8ee1f0aa37d3c55fb1043d5e92e2564029aff9fa
Mohammed <Mohammed.94822@cs.mti.edu.eg>
2024-09-25 17:59:29 +0300

    edite book page

--- Frontend/src/pages/Books/Books.jsx
+++ Frontend/src/pages/Books/Books.jsx
@@ -1,25 +1,29 @@
-import BookShow from "../../components/book/BookShow";
+import React from 'react'
+import BookShow from '../../components/book/BookShow'
+import { BsTypeH3 } from 'react-icons/bs'
 
 export default function BooksPage() {
   return (
-    <div className="flex w-{3/4} min-h-screen mx-auto bg-slate-100 py-20 ">
-      <div className="w-1/4 flex flex-col items-center bg-slate-100 ">
-        <h3>catgory</h3>
-      </div>
-      <div className="flex w-3/4 flex-wrap gap-1">
-        <BookShow />
-        <BookShow />
-        <BookShow />
-        <BookShow />
-        <BookShow />
-        <BookShow />
-        <BookShow />
-        <BookShow />
-        <BookShow />
-        <BookShow />
-        <BookShow />
-        <BookShow />
-      </div>
+    <div className='flex w-3/4 min-h-screen mx-auto bg-slate-100 py-20 items-center '>
+
+    
+
+    <div className='flex flex-wrap gap-1'>
+    <BookShow/>
+    <BookShow/>
+    <BookShow/>
+    <BookShow/>
+    <BookShow/>
+    <BookShow/>
+    <BookShow/>
+    <BookShow/>
+    <BookShow/>
+    <BookShow/>
+    <BookShow/>
+    <BookShow/>
     </div>
-  );
+    
+
+    </div>
+  )
 }
