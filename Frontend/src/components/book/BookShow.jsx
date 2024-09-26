9d0e00dab8d0389c105b66507628c735a603c3aa
Khaled_mahran_19 <136863971+kkhaled119@users.noreply.github.com>
2024-09-26 08:03:09 +0300

    edit style in book page

--- Frontend/src/components/book/BookShow.jsx
+++ Frontend/src/components/book/BookShow.jsx
@@ -1,52 +1,58 @@
-import React from 'react';
-import img from './books/hopeless.jpg'; 
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import React from "react";
+import img from "./books/hopeless.jpg";
+import { FaShoppingCart, FaSearch, FaHeart } from "react-icons/fa";
+import { BsCartPlus } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import { useBookStore } from "./../../store/bookStore";
 
 export default function BookShow() {
   return (
-    <div className='flex flex-col  h-fit cursor-pointer group
+    <div
+      className="flex flex-col h-fit cursor-pointer group
     hover:shadow-2xl hover:rounded-lg 
     hover:scale-105 hover:-translate-y-2 
-    transition-all duration-300 ease-in-out'>
-     
- <div className='w-3/4 mx-auto relative  hover:rounded-xl 
- hover:shadow-lg  hover:-translate-y-2 
- transition-all duration-300 ease-in-out group'>
-   
-   <img src={img} alt="Think and Grow Rich" 
-   className='h-full rounded-xl' />
-   
-   <div className='absolute top-3 right-3 bg-white flex 
-   flex-col space-y-2 rounded-full
-   opacity-0 group-hover:opacity-100 transition-opacity 
-   duration-300 ease-in-out '>
-     <button className='bg-white px-4 pt-3 rounded-full'>
-       <i className="fa fa-search"></i>
-     </button>
-     <button className='bg-white px-4 pb-3 rounded-full'>
-       <i className="fa fa-heart"></i>
-     </button>
-   </div>
-   <div className='absolute bottom-0 w-full px-2 opacity-0 
-   translate-y-full group-hover:opacity-100 group-hover:translate-y-0 
-   transition-all duration-700 ease-in-out bg-white rounded-b-xl'>
-     <button className='bg-white text-black px-4 py-2 w-full rounded-b-xl'>
-       Quick view
-     </button>
-</div>
-
-         
+    transition-all duration-300 ease-in-out"
+    >
+      <div
+        className="w-3/4 mx-auto relative hover:rounded-xl 
+      hover:shadow-lg hover:-translate-y-2 
+      transition-all duration-300 ease-in-out"
+      >
+        <img
+          src={img}
+          alt="Hopeless Book Cover"
+          className="h-64 rounded-xl object-cover"
+        />
+        <div
+          className="absolute top-3 right-3 bg-white flex 
+        flex-col space-y-2 rounded-full
+        opacity-0 group-hover:opacity-100 transition-opacity 
+        duration-300 ease-in-out"
+        ></div>
+        <div
+          className="absolute bottom-0 w-full px-2 opacity-0 
+        translate-y-full group-hover:opacity-100 group-hover:translate-y-0 
+        transition-all duration-700 ease-in-out bg-white rounded-b-xl"
+        >
+          <Link
+            to={""}
+            className="bg-white text-black px-4 py-2 w-full rounded-b-xl"
+          >
+            Quick view
+          </Link>
+        </div>
       </div>
 
-      <div className='py-4 px-2'>
-        <p className='text-center'>descrbtion hare name of</p>
-        <h3 className='text-center'>price</h3>
+      <div className="py-4 px-2 text-center">
+        <p className="text-sm text-gray-500">
+          Description goes here, brief book info
+        </p>
+        <h3 className="text-lg font-semibold">Price: $19.99</h3>
       </div>
 
-      
-      <div className='opacity-0 group-hover:opacity-100  transition-opacity duration-300 ease-in-out pb-2 px-2 '>
-        <button className='bg-blue-500 text-white px-4 py-2 rounded-lg w-full '>
-          Add to Cart
+      <div className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out pb-2 px-2">
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-lg w-full flex items-center justify-center gap-2">
+          <BsCartPlus size={20} className="mx-1" /> Add to Cart
         </button>
       </div>
     </div>
--- Frontend/src/pages/Books/Books.jsx
+++ Frontend/src/pages/Books/Books.jsx
@@ -1,29 +1,23 @@
-import React from 'react'
-import BookShow from '../../components/book/BookShow'
-import { BsTypeH3 } from 'react-icons/bs'
+import React from "react";
+import BookShow from "../../components/book/BookShow";
 
 export default function BooksPage() {
   return (
-    <div className='flex w-3/4 min-h-screen mx-auto bg-slate-100 py-20 items-center '>
-
-    
-
-    <div className='flex flex-wrap gap-1'>
-    <BookShow/>
-    <BookShow/>
-    <BookShow/>
-    <BookShow/>
-    <BookShow/>
-    <BookShow/>
-    <BookShow/>
-    <BookShow/>
-    <BookShow/>
-    <BookShow/>
-    <BookShow/>
-    <BookShow/>
+    <div className="flex flex-col w-full min-h-screen mx-auto bg-slate-100 py-20">
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 w-3/4 mx-auto">
+        <BookShow />
+        <BookShow />
+        <BookShow />
+        <BookShow />
+        <BookShow />
+        <BookShow />
+        <BookShow />
+        <BookShow />
+        <BookShow />
+        <BookShow />
+        <BookShow />
+        <BookShow />
+      </div>
     </div>
-    
-
-    </div>
-  )
+  );
 }
