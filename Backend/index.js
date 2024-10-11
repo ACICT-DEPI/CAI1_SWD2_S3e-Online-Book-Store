8a66f2f6344680908030abda30063aa10e945350
Mahmoud Galal <mahmoudgalal173.95@gmail.com>
2024-10-11 17:18:28 +0300

    Finish analytics page with backend and ui

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use("uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use(express.json());
